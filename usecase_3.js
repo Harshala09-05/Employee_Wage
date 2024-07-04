e59e8a9e968fe6cfe6ee7e7c10d887a6c43da08d
Harshala09-05 <pharshala03@gmail.com>
2024-07-04 14:59:05 +0530

    Update usecase_3.js

--- usecase_3.js
+++ usecase_3.js
@@ -1,6 +1,8 @@
+//calculate Part time wage of employee. 
 function calculateWedge(wagePerHour,HoursWorked,partTimeHour) {
     
   const totalWedge = wagePerHour * (HoursWorked + partTimeHour);
     console.log("wage of employee: " + totalWedge);
 };
+//call function
 calculateWedge(20,8,8);
